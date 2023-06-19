5aa69293af0bf34ab80026819f0ae9a42735ea3b
movingChurch <seong9115@gmail.com>
2023-06-19 14:07:29 +0900

    feat/Fixed gtest version

--- thirdparty/gtest/set_gtest.cmake
+++ thirdparty/gtest/set_gtest.cmake
@@ -11,7 +11,7 @@ else()
   ExternalProject_Add(
     GTEST
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.13.0
+    GIT_TAG release-1.12.1
     PREFIX ${CMAKE_BINARY_DIR}/gtest-prefix
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install/${CMAKE_BUILD_TYPE}
   )
